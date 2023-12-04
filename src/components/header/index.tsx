996d521884c1560d76deb633b73049d693ebe815
leandroricke100 <leandro_henrique_100@hotmail.com>
2023-12-04 19:31:38 -0300

    update

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -13,25 +13,23 @@ export function Header() {
         <Link to="/">
           <img src={logoImg} alt="Logo do site" />
         </Link>
+        <Link className="relative" to="/itemcart">
+          <div>
+            <FiShoppingCart size={22} color="#000" />
+            {cartAmount > 0 && (
+              <span className="absolute -top-3 -right-3 px-2.5b bg-red-500 rounded-full w-5 h-5 flex items-center justify-center text-white text-sm">
+                {cartAmount}
+              </span>
+            )}
+          </div>
+        </Link>
 
         {!loadingAuth && signed && (
-          <>
-            <Link className="relative" to="/itemcart">
-              <div>
-                <FiShoppingCart size={22} color="#000" />
-                {cartAmount > 0 && (
-                  <span className="absolute -top-3 -right-3 px-2.5b bg-red-500 rounded-full w-5 h-5 flex items-center justify-center text-white text-sm">
-                    {cartAmount}
-                  </span>
-                )}
-              </div>
-            </Link>
-            <Link to="/dashboard">
-              <div className="border-2 rounded-full p-1 border-gray-900">
-                <FiUser size={22} color="#000" />
-              </div>
-            </Link>
-          </>
+          <Link to="/dashboard">
+            <div className="border-2 rounded-full p-1 border-gray-900">
+              <FiUser size={22} color="#000" />
+            </div>
+          </Link>
         )}
 
         {!loadingAuth && !signed && (
